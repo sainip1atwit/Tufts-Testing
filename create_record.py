1d4dc818f30675c83310c1923d0f94e338681bf3
sainip1atwit <sainip1@wit.edu>
2025-06-30 19:58:25 -0400

    Updated requirements and create_record script. Create record script opens new asset form

--- create_record.py
+++ create_record.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from dotenv import load_dotenv
+import timeunit
 
 # Load the .env file
 load_dotenv()
@@ -12,8 +13,8 @@ load_dotenv()
 # Start the chrome web driver
 driver = webdriver.Chrome()
 
-# Access TechConnect (Auth)
-driver.get('http://tufts.service-now.com')
+# Access New Computer Asset Form
+driver.get('https://tufts.service-now.com/now/nav/ui/classic/params/target/alm_hardware.do%3Fsys_id%3D-1%26sys_is_list%3Dtrue%26sys_target%3Dalm_hardware%26sysparm_checked_items%3D%26sysparm_fixed_query%3D%26sysparm_group_sort%3D%26sysparm_list_css%3D%26sysparm_query%3Dmodel_category%253d81feb9c137101000deeabfc8bcbe5dc4%26sysparm_referring_url%3Dalm_hardware_list.do%253fsysparm_query%253dmodel_category%25253D81feb9c137101000deeabfc8bcbe5dc4%25255EEQ%26sysparm_target%3D%26sysparm_view%3D')
 
 # Set up for the auth redirect
 username = driver.find_element(By.ID, 'username')
@@ -27,10 +28,20 @@ password.send_keys(os.getenv('TUFTSPASS'))
 # Submit login
 submit.click()
 
-# Click "Skip For Now" after logging in
-
-WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH, '/html/body/div/div/div/div[3]/div/button'))
-)
-
-time.sleep(30)
+# Catch 'Skip For Now' Element in new page
+try:
+    skip_for_now = WebDriverWait(driver, 30).until(
+        EC.presence_of_element_located((By.XPATH, '/html/body/div/div/div/div[3]/div/button'))
+    )
+    skip_for_now.click()
+finally:
+    # Catch 'Don't Trust Browser' Element
+    try:
+        dont_trust = WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="dont-trust-browser-button"]'))
+        )
+        dont_trust.click()
+    finally:
+        # 30 seconds until driver closes
+        time.sleep(30)
+        driver.quit()
