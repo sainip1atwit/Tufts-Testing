aacda7e08823cfe7175e055bf49963ddfaf94a45
sainip1atwit <sainip1@wit.edu>
2025-07-02 01:07:31 -0400

    Updated create_record

--- scripts/create_record.py
+++ scripts/create_record.py
@@ -14,7 +14,8 @@ load_dotenv()
 driver = webdriver.Chrome()
 
 # Access New Computer Asset Form
-driver.get('https://tufts.service-now.com/now/nav/ui/classic/params/target/alm_hardware.do%3Fsys_id%3D-1%26sys_is_list%3Dtrue%26sys_target%3Dalm_hardware%26sysparm_checked_items%3D%26sysparm_fixed_query%3D%26sysparm_group_sort%3D%26sysparm_list_css%3D%26sysparm_query%3Dmodel_category%253d81feb9c137101000deeabfc8bcbe5dc4%26sysparm_referring_url%3Dalm_hardware_list.do%253fsysparm_query%253dmodel_category%25253D81feb9c137101000deeabfc8bcbe5dc4%25255EEQ%26sysparm_target%3D%26sysparm_view%3D')
+HARDWAREURL = os.getenv('HARDWAREURL')
+driver.get(HARDWAREURL)
 
 # Set up for the auth redirect
 username = driver.find_element(By.ID, 'username')
@@ -29,18 +30,21 @@ password.send_keys(os.getenv('TUFTSPASS'))
 submit.click()
 
 # Catch 'Skip For Now' Element in new page
-try:
-    skip_for_now = WebDriverWait(driver, 30).until(
-        EC.presence_of_element_located((By.XPATH, '/html/body/div/div/div/div[3]/div/button'))
-    )
-    skip_for_now.click()
-finally:
-    # Catch 'Don't Trust Browser' Element
-    try:
-        dont_trust = WebDriverWait(driver, 30).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="dont-trust-browser-button"]'))
-        )
-        dont_trust.click()
-    finally:
-        time.sleep(30)
-        
+skip_for_now = WebDriverWait(driver, 30).until(
+    EC.presence_of_element_located((By.XPATH, '/html/body/div/div/div/div[3]/div/button'))
+)
+skip_for_now.click()
+
+# Catch 'Don't Trust Browser' Element
+dont_trust = WebDriverWait(driver, 30).until(
+    EC.presence_of_element_located((By.XPATH, '//*[@id="dont-trust-browser-button"]'))
+)
+dont_trust.click()
+
+# Switch to frame
+WebDriverWait(driver, 30).until(lambda d: d.current_url == HARDWAREURL)
+driver.switch_to.default_content()
+driver.implicitly_wait(10)
+driver.switch_to.frame('gsft_main')
+
+time.sleep(100)
