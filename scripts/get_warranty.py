80f14fc182e0ff37713fcdc6adb8d843e17a6db8
Prab Saini <sainip1@wit.edu>
2025-07-02 10:45:21 -0400

    Get Warranty info on computer

--- scripts/get_warranty.py
+++ scripts/get_warranty.py
@@ -2,6 +2,7 @@ import time
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -12,29 +13,41 @@ df = pd.read_excel(ASSET_SHEET)
 initial_row = df.iloc[0]
 print(initial_row['Serial Number'], initial_row['Asset Tag'])
 
+# Grab data from excel sheet
+manufacturer = initial_row['Manufacturer']
+serial_number = initial_row['Serial Number']
+model_number = initial_row['Model']
+
 # Chrome Web Driver
 driver = webdriver.Chrome()
 
-# Access Dell Warranty Site
-driver.get('https://www.dell.com/support/contractservices/en-us')
+# Access Warranty Site
+driver.get('http://www.lookupwarranty.com/')
 
-try:
-    # Find the warranty lookup input
-    warranty_lookup = WebDriverWait(driver, 30).until(
-        EC.presence_of_element_located((By.XPATH, '//*[@id="homemfe-dropdown-input"]'))
-    )
-    warranty_lookup.send_keys(initial_row['Serial Number'])
-finally:
-    # Click search
-    search_button = driver.find_element(By.XPATH, '//*[@id="btnSubmit"]')
-    search_button.click()
+# Grab the elements from the site
+mfg = Select(driver.find_element(By.ID, 'mfg'))
+serial = driver.find_element(By.ID, 'serial')
+model = driver.find_element(By.ID, 'model')
+submit = driver.find_element(By.ID, 'submitButton')
 
-    # Wait for information to load
-    try:
-        driver.implicitly_wait(10)
-        review_services = WebDriverWait(driver, 30).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="mfe-productdetails"]/div[2]/div[2]/div[2]/div/div[1]/div[4]/div[1]/a/button'))
-        )
-        review_services.click()
-    finally:
-        time.sleep(30)
+# Interact with Site
+mfg.select_by_value(manufacturer.lower())
+serial.send_keys(serial_number)
+model.send_keys(model_number)
+
+# Click Submit?
+submit.click()
+
+WebDriverWait(driver, 30).until(
+    EC.presence_of_element_located((By.XPATH, '//*[@id="output"]/table'))
+)
+
+warranty_start = driver.find_element(By.XPATH, '//*[@id="output"]/table/tbody/tr[3]/td[2]')
+warranty_end = driver.find_element(By.XPATH, '//*[@id="output"]/table/tbody/tr[4]/td[2]')
+
+print(type(warranty_start.text))
+
+'''df.loc[df['Serial Number'] == serial_number, 'Warranty Start'] = warranty_start.text
+df.loc[df['Serial Number'] == serial_number, 'Warranty End'] = warranty_end.text'''
+
+time.sleep(100)
