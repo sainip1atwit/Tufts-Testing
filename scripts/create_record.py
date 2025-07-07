2e7456367816eb18b9af37991faa851ef7294409
Prab Saini <sainip1@wit.edu>
2025-07-07 18:20:04 -0400

    Fix TC Record Creation

--- scripts/create_record.py
+++ scripts/create_record.py
@@ -15,7 +15,7 @@ driver = webdriver.Chrome()
 
 # Access New Computer Asset Form
 HARDWAREURL = os.getenv('HARDWAREURL')
-driver.get(HARDWAREURL)
+driver.get(str(HARDWAREURL))
 
 # Set up for the auth redirect
 username = driver.find_element(By.ID, 'username')
@@ -41,10 +41,30 @@ dont_trust = WebDriverWait(driver, 30).until(
 )
 dont_trust.click()
 
-# Switch to frame
-WebDriverWait(driver, 30).until(lambda d: d.current_url == HARDWAREURL)
-driver.switch_to.default_content()
-driver.implicitly_wait(10)
-driver.switch_to.frame('gsft_main')
+time.sleep(3)
+#[!]WTF Shadow-root BS might be worse than iframe nonsense.
+# Shadow host is top level shadow root object (find by CSS only)
+shadow_host = driver.find_element(By.CSS_SELECTOR, 'body > macroponent-f51912f4c700201072b211d4d8c26010')
+time.sleep(3)
+shadow_root = shadow_host.shadow_root
+
+time.sleep(3)
+#[!]switch to the proper iframe (iframes suck); in this case the whole inner page is an iframe, remember this!
+# Worth noting that only new way of finding elements (find_element(By.Something, 'something')) works with shadowDOM methods
+iframe = shadow_root.find_element(By.ID, 'gsft_main')
+driver.switch_to.frame(iframe)
+
+time.sleep(3)
+
+# Get Record Information Page 1
+asset = driver.find_element(By.XPATH, '//*[@id="alm_hardware.asset_tag"]')
+managed = driver.find_element(By.XPATH, '//*[@id="sys_display.alm_hardware.managed_by"]')
+serial = driver.find_element(By.XPATH, '//*[@id="alm_hardware.serial_number"]')
+model = driver.find_element(By.XPATH, '//*[@id="sys_display.alm_hardware.model"]')
+
+# Contracts Page
+contracts = driver.find_element(By.XPATH, '//*[@id="tabs2_section"]/span[4]/span[1]')
+warranty_start = driver.find_element(By.XPATH, '//*[@id="alm_hardware.u_warranty_start"]')
+warranty_end = driver.find_element(By.XPATH, '//*[@id="alm_hardware.warranty_expiration"]')
 
 time.sleep(100)
